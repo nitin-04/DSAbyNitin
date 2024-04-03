426428d0a3c5c472c921227cbebd417bd72a5c48
nitin bahugun <nitinbahuguna1251@gmail.com>
2024-04-03 11:28:24 +0530

    bubble sorting

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int bubble(int arr[], int size){
     // for round 1 to size of the array
     for(int i=0; i<=size; i++){
-        for(int j = 0; j < size-1; j++){
-
+        for(int j = 0; j < size-i; j++){
+    // j<size because  we are comparing current element with next one. and when j becomes last element of the array, so there is no j+1.
+    // size - i because after every round j is decrease by 1,2 ...
             if (arr[j]  > arr[j+1]){
                 //  swap elements if the current element is greater than next
                 swap(arr[j], arr[j+1]); 
